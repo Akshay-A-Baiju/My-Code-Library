8ed05dc959218c56fb66415df844adf5d48d1dcf
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2021-09-22 14:52:36 +0530

    Create template1.cpp

---
+++ CP Templates/template1.cpp
@@ -0,0 +1,58 @@
+// Author : Akshay A Baiju
+// Problem link : 
+// Problem tags : 
+// TC :
+// SC : 
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll                  long long
+#define ull                 unsigned long long
+#define vi                  vector <int>
+#define vll                 vector <ll>
+#define vvll                vector <vll>
+#define pii                 pair <int,int>
+#define ppi                 pair <int,pii>
+#define pll                 pair <ll,ll>
+#define ppl                 pair <ll,pll>
+#define ms(a,x)             memset (a,x,sizeof(a))
+#define rep(i,a,n)          for (int i=a;i<n;i++)
+#define rrep(i,n,a)         for (int i=n;i>=a;i--)
+#define pb                  push_back
+#define pf                  push_front
+#define ppb                 pop_back
+#define ff                  first
+#define ss                  second
+#define ins                 insert
+#define umap                unordered_map
+#define uset                unordered_set
+#define YY					        cout<<"YES"
+#define NN					        cout<<"NO"
+#define all(v)              v.begin(), v.end()
+#define desc()              greater <ll>()
+#define endl                "\n"   //not to be used in interactive problems
+#define scan_vector(v)      for (auto &it: v) { cin>>it; }
+#define print_vector(v)     for (auto &it: v) { cout<<it<<" "; } cout<<endl;
+#define print_map(mp)       for (auto &pr: mp) { cout<<pr.first<<" "<<pr.second<<endl; }
+#define sync                ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+const int M = 1e9+7;
+
+void solve()
+{    
+
+}
+
+int main()
+{
+	// clock_t clk = clock();
+	sync;
+	int t=1;
+	cin>>t;
+	while (t--)
+	{
+		solve();
+		cout<<endl;
+	}
+	// cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
+	return 0;
+}

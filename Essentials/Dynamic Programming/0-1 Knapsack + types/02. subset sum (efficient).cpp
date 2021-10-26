448df9c9575e169d8211a3cecc00b116ac3c21ee
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2021-10-26 22:49:29 +0530

    Update 02. subset sum (efficient).cpp
    #dp
    #knapsack

--- Essentials/Dynamic Programming/0-1 Knapsack + types/02. subset sum (efficient).cpp
+++ Essentials/Dynamic Programming/0-1 Knapsack + types/02. subset sum (efficient).cpp
@@ -3,6 +3,26 @@
 using namespace std;
 #define ll long long
 #define vll vector<ll>
+
+bool subset_sum(vll &v, ll sum, ll n)
+{
+	vector <vector<bool>> dp(n+1, vector<bool> (sum+1,false));
+	//initialization
+	dp[0][0]=true;
+	//top-down dp
+	for (ll i=1;i<=n;i++)
+	{
+		for (ll j=0;j<=sum;j++)
+		{
+			if (j-v[i-1]>=0)
+				dp[i][j] = dp[i-1][j] | dp[i-1][j-v[i-1]];
+			else
+				dp[i][j] = dp[i-1][j];
+		}
+	}
+	return dp[n][sum];
+}
+
 int main()
 {
 	ll n,sum;
@@ -10,22 +30,6 @@ int main()
 	vll v(n);
 	for (ll i=0;i<n;i++)
 		cin>>v[i];
-	//using top down Dp -> Linear space
-	vector <bool> dp(sum+1,false);
-	//initialization
-	dp[0]=true;
-	for (ll i=0;i<n;i++)
-	{
-		//traverse from back of dp array
-		for (ll j=sum;j>=0;j--)
-		{
-			if (dp[j]==false)
-			{
-				if (j-v[i]>=0 && dp[j-v[i]]==true)
-					dp[j]=true;
-			}
-		}
-	}
-	dp[sum]?cout<<"True":cout<<"False";
+	subset_sum(v,sum,n)?cout<<"yes":cout<<"no";
 	return 0;
 }

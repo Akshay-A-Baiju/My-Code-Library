38744bffa539a6e269f96b13c274dc685f321e8c
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2021-10-26 22:50:55 +0530

    Update 02. subset sum (inefficient).cpp
    #dp
    #knapsack

--- Essentials/Dynamic Programming/0-1 Knapsack + types/02. subset sum (inefficient).cpp
+++ Essentials/Dynamic Programming/0-1 Knapsack + types/02. subset sum (inefficient).cpp
@@ -2,13 +2,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-int main()
+#define vll vector<ll>
+
+bool subset_sum(vll &v, ll sum, ll n)
 {
-	ll n,sum;
-	cin>>n>>sum;
-	vll v(n);
-	for (ll i=0;i<n;i++)
-		cin>>v[i];
 	vector <vector<bool>> dp(n+1, vector<bool> (sum+1,false));
 	//initialization
 	dp[0][0]=true;
@@ -23,6 +20,15 @@ int main()
 				dp[i][j] = dp[i-1][j];
 		}
 	}
-	dp[n][sum]?cout<<"True":cout<<"False";
+	return dp[n][sum];
+}
+int main()
+{
+	ll n,sum;
+	cin>>n>>sum;
+	vll v(n);
+	for (ll i=0;i<n;i++)
+		cin>>v[i];
+	subset_sum(v,sum,n)?cout<<"yes":cout<<"no";
 	return 0;
 }

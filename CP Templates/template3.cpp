0a8035b01c4c52fd7547be5fc3e95d3ad1052a8b
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2022-02-19 11:51:33 +0530

    Update template3.cpp

--- CP Templates/template3.cpp
+++ CP Templates/template3.cpp
@@ -1,44 +1,52 @@
-// Author : Akshay A Baiju
-// Problem link : 
-// Contest : 
-// Problem tags : 
-// TC :
-// SC : 
+// Author: Akshay A Baiju
+// Problem: 
+// Contest: 
+// URL: 
+// Memory Limit: 
+// Time Limit: 
+// Problem tag: 
+// TC: 
+// SC: 
 
 #include "bits/stdc++.h"
-// #include<ext/pb_ds/assoc_container.hpp>
-// #include<ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
-// using namespace __gnu_pbds;
+using namespace __gnu_pbds;
 
-#define ll                  long long
-#define ull                 unsigned long long
-#define ld		    long double
-#define vi                  vector <int>
-#define vll                 vector <ll>
-#define vvll                vector <vll>
-#define pii                 pair <int,int>
-#define pll                 pair <ll,ll>
-#define vpll                vector <pll>
-#define rep(i,a,n)          for (int i=a;i<n;i++)
-#define rrep(i,n,a)         for (int i=n;i>=a;i--)
-#define fill1(a,x)          for (auto &it: a) it=x;
-#define fill2(a,x)	    for (auto &v: a) { for (auto &it: v) it=x; }
-#define pb                  push_back
-#define pf                  push_front
-#define ppb                 pop_back
-#define ff                  first
-#define ss                  second
-#define ins                 insert
-#define YY		    cout<<"YES"
-#define NN		    cout<<"NO"
-#define all(v)              v.begin(), v.end()
-#define allr(v)		    v.rbegin(), v.rend()
-#define desc()              greater <ll>()
-#define endl                "\n"   //not to be used in interactive problems
-#define sync                ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-// #define oset		          tree <ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update> // find_by_order, order_of_key
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define vi vector <int>
+#define vll vector <ll>
+#define vvll vector <vll>
+#define vbool vector <bool>
+#define pii pair <int,int>
+#define pll pair <ll,ll>
+#define vpll vector <pll>
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define rrep(i,n,a) for (int i=n;i>=a;i--)
+#define fill1(a,x) for (auto &it: a) it=x;
+#define fill2(a,x) for (auto &v: a) { for (auto &it: v) it=x; }
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ff first
+#define ss second
+#define ins insert
+#define YY cout<<"YES"
+#define NN cout<<"NO"
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+#define desc() greater <ll>()
+#define endl "\n"   //not to be used in interactive problems
+#define random(l,r,T) uniform_int_distribution<T>(l,r)(rng)
+#define sync ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define oset tree <ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update> // find_by_order, order_of_key
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
 #ifndef ONLINE_JUDGE
 	#define debug(x)	    cerr<<#x<<" = "; _print(x); cerr<<"\n";
 #else
@@ -54,11 +62,11 @@ const ld eps = 1e-6;
 int dx[]={0,1,0,-1};	//{0,1,1,1,0,-1,-1,-1};
 int dy[]={1,0,-1,0};	//{1,1,0,-1,-1,-1,0,1};
 
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(ld t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
+void _print(ll t) { cerr<<t; }
+void _print(int t) { cerr<<t; }
+void _print(ld t) { cerr<<t; }
+void _print(string t) { cerr<<t; }
+void _print(char t) { cerr<<t; }
 
 template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
 template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
@@ -71,14 +79,15 @@ template<typename T> ostream& operator <<(ostream &out,const vector<T> &v){ for(
 template<typename T1,typename T2> istream& operator >>(istream &in,pair<T1,T2> &p){ in>>p.first>>p.second; return in;}
 template<typename T1,typename T2> ostream& operator <<(ostream &out,const pair<T1,T2> &p){ out<<p.first<<' '<<p.second; return out;}
 
-ll gcd(ll a, ll b) 	       {while (b) {a %= b; swap(a,b);} return a;}
-ll lcm(ll a, ll b) 	       {ll g=gcd(a,b); ll res=a*(b/g); return res;}
-ll binExp(ll a, ll b, ll m=M)  {a = a % m; ll res = 1; while (b) {if (b&1) {res=(res * a) % m;} a=(a * a) % m; b>>=1;} return res;}
-ll mod_add(ll a, ll b, ll m=M) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-ll mod_sub(ll a, ll b, ll m=M) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-ll mod_mul(ll a, ll b, ll m=M) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
-ll mod_inv(ll a, ll m=M)       {a = a % m; return binExp(a,m-2,m);}		// only for prime m
-ll mod_div(ll a, ll b, ll m=M) {a = a % m; ll binv = mod_inv(b,m); return (((a * binv) % m) + m) % m;}
+ll gcd(ll a, ll b) { while (b) {a %= b; swap(a,b);} return a; }
+ll lcm(ll a, ll b) { ll g=gcd(a,b); ll res=a*(b/g); return res; }
+ll extended_gcd(ll a, ll b, ll &x, ll &y) { if (b==0) { x=1; y=0; return a; } ll x1,y1; ll g=extended_gcd(b,a%b,x1,y1); x=y1; y=x1-y1*(a/b); return g; }
+ll binExp(ll a, ll b, ll m=M) { a = a % m; ll res = 1; while (b) { if (b&1) { res=(res * a) % m; } a=(a * a) % m; b>>=1; } return res; }
+ll mod_inv(ll a, ll m=M) { a = a % m; return binExp(a,m-2,m); }		// only for prime m
+ll mod_add(ll a, ll b, ll m=M) { a = a % m; b = b % m; return (((a + b) % m) + m) % m; }
+ll mod_sub(ll a, ll b, ll m=M) { a = a % m; b = b % m; return (((a - b) % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m=M) { a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+ll mod_div(ll a, ll b, ll m=M) { a = a % m; ll binv = mod_inv(b,m); return (((a * binv) % m) + m) % m; }
 
 struct custom_hash
 {
@@ -102,7 +111,6 @@ template <class T> using uset=unordered_set<T,custom_hash>;
 void solve()
 {
 	
-
 }
 
 int main()
